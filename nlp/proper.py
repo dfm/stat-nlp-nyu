f0ffd598a612c98f8395020b5828cf9cdfb65fb8
Dan F-M <danfm@nyu.edu>
2013-09-09 17:48:45 -0400

    working bigram model

--- nlp/proper.py
+++ nlp/proper.py
@@ -9,6 +9,10 @@ import numpy as np
 from collections import defaultdict
 
 
+START = "|"
+STOP = "\\"
+
+
 class Distribution(object):
 
     def __init__(self):
@@ -52,15 +56,17 @@ class UnigramModel(object):
             self.prior.incr(c)
             [self.distributions[c].incr(l.lower()) for l in w if l != " "]
 
-    def classify(self, word):
-        # return "movie", 0.2
+    def get_probabilities(self, word):
         prob = defaultdict(lambda: 1.0)
         for char in word.lower():
             if char == " ":
                 continue
             for c, dist in self.distributions.items():
                 prob[c] *= dist[char]
+        return prob
 
+    def classify(self, word):
+        prob = self.get_probabilities(word)
         classes, probabilities = zip(*(prob.items()))
         ind = np.argmax(probabilities)
         norm = np.sum(probabilities)
@@ -89,7 +95,29 @@ class UnigramModel(object):
 
 class BigramModel(UnigramModel):
 
-    pass
+    def __init__(self, training_set, f2=0.9):
+        super(BigramModel, self).__init__(training_set)
+        self.bigrams = dict([(c, defaultdict(Distribution))
+                             for c in training_set.classes])
+        for cls, word in training_set:
+            dist = self.bigrams[cls]
+            for i, char in enumerate(START+word):
+                (dist[char.lower()].incr(word[i].lower())
+                 if i < len(word) else dist[char.lower()].incr(STOP))
+
+    def get_probabilities(self, word):
+        prob = defaultdict(lambda: 1.0)
+        for i, char in enumerate(START+word.lower()):
+            for cls, dist in self.bigrams.items():
+                if char not in dist:
+                    prob[cls] = 0.0
+                    continue
+
+                if i < len(word):
+                    prob[cls] *= dist[char][word[i].lower()]
+                else:
+                    prob[cls] *= dist[char][STOP]
+        return prob
 
 
 if __name__ == "__main__":
@@ -97,7 +125,8 @@ if __name__ == "__main__":
     validation_set = Dataset("data/pnp-validate.txt")
     test_set = Dataset("data/pnp-test.txt")
 
-    model = UnigramModel(training_set)
+    model = BigramModel(training_set)
+    # model = UnigramModel(training_set)
     print("Validation accuracy: {0}".format(
         model.test(validation_set)))
     model.test(test_set, outfile="hw2/output.txt")
